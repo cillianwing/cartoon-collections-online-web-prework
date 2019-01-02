143f9abab7db3aa972829104823a9ceda46849c4
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-02 17:51:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,4 +19,6 @@ end
 def find_the_cheese(strings_array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings_array.each do |sub_array|
+    sub_array.include
 end
