72e4a3b344f5b1a2b66a34d140c079827fba2d64
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-02 17:08:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls_array)# code an argument here
   
 end
 
